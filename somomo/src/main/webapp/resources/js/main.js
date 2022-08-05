3632dc732fa7a70cbd28e6e0e8577d8c80eba6ae
jjaong <agyh8491@gmail.com>
2022-08-05 16:10:39 +0900

    윤자원
    테스트

--- somomo/src/main/webapp/resources/js/main.js
+++ somomo/src/main/webapp/resources/js/main.js
@@ -1,14 +1,19 @@
 // 선택자 정의
+
 const body = document.querySelector("body"),
       sidebar = body.querySelector("nav");
       sidebarToggle = body.querySelector(".sidebar-toggle");
 
+
 // 왼쪽 바 아이콘 클릭했을 떄 클래스 close 추가
+
 sidebarToggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
+
 // 프로필 DROPDOWN
+
 const profile = document.querySelector('.profile');
 const dropdownProfile = profile.querySelector('.profile-link');
 
@@ -17,7 +22,9 @@ profile.addEventListener('click', function() {
 })
 
 // 윈도우 화면에서 다른곳 클릭했을 때 닫히게 하기
+
 window.addEventListener('click', function(e){
+
     if(e.target !== profile) {
         if(e.target !== dropdownProfile) {
             if(dropdownProfile.classList.contains('show')) {
